9518fc0f2c5cb4d4b05a5838a6454a35d72faabc
AMAN0440DE <148725980+AMAN0440DE@users.noreply.github.com>
2025-06-21 23:25:48 +0530

    Add files via upload

---
+++ typePromotion.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class typePromotion {
+    public static void main(String args[]){
+        Scanner K = new Scanner(System.in);
+        // String x = K.next();
+        // char a ='a';
+        // char b = 'b';
+        // System.out.println(b-a);
+
+        float c = 23.4f;
+        long d = 345678;
+        double e = 123.43;
+        double result = c+d+e;      // accepted
+        // int result = c+d+e;            // will lossy conversion
+
+
+
+    }
+}
